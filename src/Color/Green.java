c9ee7067a62cf1c70b5b9651e15d23f6eca2ec60
Himaglahi <mjobsidmagsalos@gmsil.com>
2023-11-29 18:50:32 +0800

    Color

---
+++ src/Color/Green.java
@@ -0,0 +1,12 @@
+package Color;
+
+public class Green extends Item{
+
+    Green(String item){
+        super(item);
+    }
+
+    void Color(){
+        System.out.println(item + " is color Green");
+    }
+}
---
+++ src/Color/Main.java
@@ -0,0 +1,15 @@
+package Color;
+
+public class Main {
+   
+    public static void main(String[] args) {
+
+        Green bag = new Green("Nike Bag");
+        Blue shoes = new Blue("Nike Shoes");
+        Yellow shirt = new Yellow("Nike Shirt");
+        
+        bag.Color();
+        shoes.Color();
+        shirt.Color();
+    }
+}
